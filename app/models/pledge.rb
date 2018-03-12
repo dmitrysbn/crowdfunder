9bf1849e577607b69c11ff20e568c4818d31b0a0
aubanuet <aubanuet@gmail.com>
2018-03-12 15:44:57 -0400

    Add validate check_owner

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,12 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :check_owner
+
+  def check_owner
+      if user == project.user
+        errors.add(:user, "must not be the owner of the project")
+      end
+  end
+
 end
